681adcff4365667dce509b2ba1b076771b4bd12e
Henric Åkesson <henric@bravissimo.se>
2018-09-24 13:02:25 +0200

    Gjort komponenten "stayWithUs" responsiv.

--- components/StayWithUs.vue
+++ components/StayWithUs.vue
@@ -69,6 +69,8 @@ export default {
 
 <style lang="scss" scoped>
 $gutter: 35px;
+$gutterLaptop: 30px;
+$gutterSmallTablet: 25px;
 
 .stayWithUs {
     text-align: center;
@@ -76,9 +78,27 @@ $gutter: 35px;
     padding: 97px 0 114px;
     margin-left: -50px;
     margin-right: -50px;
+    @media ($tablet) {
+        padding: 90px 0;
+    }
+    @media ($smallTablet) {
+        padding: 70px 0;
+    }
+    @media ($mobile) {
+        padding: 50px 0;
+    }
 }
 .top {
     margin-bottom: 60px;
+    @media ($tablet) {
+        margin-bottom: 50px;
+    }
+    @media ($smallTablet) {
+        margin-bottom: 40px;
+    }
+    @media ($mobile) {
+        margin-bottom: 30px;
+    }
     .preamble {
         margin-top: 16px;
     }
@@ -90,10 +110,51 @@ $gutter: 35px;
     margin-left: -$gutter;
     margin-right: -$gutter;
     padding: 0px 150px;
+    @media ($largeDesktop) {
+        padding: 0 50px;
+    }
+    @media ($laptop) {
+        padding: 0;
+        margin-left: -$gutterLaptop;
+        margin-right: -$gutterLaptop;
+    }
+    @media ($largeTablet) {
+        margin-bottom: -50px;
+    }
+    @media ($smallTablet) {
+        margin-left: -$gutterSmallTablet;
+        margin-right: -$gutterSmallTablet;
+    }
+    @media ($smallerTablet) {
+        margin-left: auto;
+        margin-right: auto;
+        width: 360px;
+        max-width: 100%;
+    }
+    @media ($mobile) {
+        margin-bottom: -30px;
+    }
 }
 .column {
     width: 25%;
     padding: 0 $gutter;
+    @media ($laptop) {
+        padding: 0 $gutterLaptop;
+    }
+    @media ($largeTablet) {
+        width: 50%;
+        margin-bottom: 50px;
+    }
+    @media ($smallTablet) {
+        padding: 0 $gutterSmallTablet;
+    }
+    @media ($smallerTablet) {
+        width: 100%;
+        padding: 0;
+    }
+    @media ($mobile) {
+        margin-bottom: 30px;
+    }
 }
 .iconHolder {
     display: flex;
@@ -106,23 +167,72 @@ $gutter: 35px;
     padding-bottom: 10px;
     border-radius: 100%;
     border: 8px solid #f4f8fa;
+    @media ($laptop) {
+        width: 200px;
+        height: 200px;
+    }
+    @media ($smallTablet) {
+        width: 180px;
+        height: 180px;
+        border-width: 5px;
+    }
+    @media ($smallerTablet) {
+        width: 160px;
+        height: 160px;
+        margin-bottom: 30px;
+    }
+    @media ($mobile) {
+        width: 140px;
+        height: 140px;
+    }
 }
 .icon {
     &--person {
         width: 37px;
         height: 78px;
+        @media ($smallTablet) {
+            width: 27px;
+            height: 58px;
+        }
+        @media ($mobile) {
+
+        }
     }
     &--thumbUp {
         width: 66px;
         height: 66px;
+        @media ($smallTablet) {
+            width: 56px;
+            height: 56px;
+        }
+        @media ($mobile) {
+            width: 46px;
+            height: 46px;
+        }
     }
     &--house {
         width: 76px;
         height: 82px;
+        @media ($smallTablet) {
+            width: 58px;
+            height: 62px;
+        }
+        @media ($mobile) {
+            width: 50px;
+            height: 54px;
+        }
     }
     &--checklist {
         width: 55px;
         height: 79px;
+        @media ($smallTablet) {
+            width: 45px;
+            height: 64px;
+        }
+        @media ($mobile) {
+            width: 38px;
+            height: 54px;
+        }
     }
 }
 .stepHeading {
@@ -131,6 +241,9 @@ $gutter: 35px;
 .stepText {
     font-size: 16px;
     line-height: 1.65;
+    @media ($mobile) {
+        font-size: 14px;
+    }
 }
 .stepNumber {
     position: absolute;
@@ -148,6 +261,13 @@ $gutter: 35px;
     display: flex;
     align-items: center;
     justify-content: center;
+    @media ($mobile) {
+        width: 30px;
+        height: 30px;
+        margin-left: -15px;
+        margin-top: -13px;
+        font-size: 15px;
+    }
 }
 
 </style>
