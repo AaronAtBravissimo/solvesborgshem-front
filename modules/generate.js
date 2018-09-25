dd7cffbaf09676c4238357d64fe1b890a411b366
Kim Larsson <kimla89@gmail.com>
2018-09-25 10:06:35 +0200

    Snyggat till koden för generate.js.

--- modules/generate.js
+++ modules/generate.js
@@ -1,37 +1,37 @@
-const axios = require( 'axios' )
-const Cache = require( 'node-persist' );
-const cache = Cache.create( { dir: '/tmp/', ttl: 300000 } );
-const fs = require( 'fs' )
-const ef = require( 'empty-folder' )
-const debug = require( 'debug' )( 'nuxt:generate' )
+const axios = require('axios')
+const fs = require('fs' )
+const ef = require('empty-folder')
+const debug = require('debug')('nuxt:generate')
 
 const config = require('../utils/config');
 const apiUrl = config.apiUrl;
 const baseUrl = config.baseUrl;
 
 module.exports = function () {
-	this.nuxt.hook('generate:before', async ( generator ) => {
+	this.nuxt.hook('generate:before', async (generator) => {
 		const routes = async () => {
-            ef( generator.nuxt.options.generate.apiCacheDir, false, ( feedback ) => {
-                if ( ! feedback.error ) {
-                    debug( 'Stale API cache removed' )
+            ef(generator.nuxt.options.generate.apiCacheDir, false, (feedback) => {
+                if (!feedback.error) {
+                    debug('Stale API cache removed');
                 }
-            } )
+            })
 
-            const pages = axios
-            .get(`${apiUrl}/api/page`)
-            .then(res => {
-                res.data.map(page => {
-                    let name = page.post_link.replace(baseUrl, '');
-                    name = name.replace(/\//g, '-_-');
-                    name = name.replace(/\\/g, '-_-');
-                    const path = (generator.nuxt.options.generate.apiCacheDir + '/' + name + '.json');
-                    fs.writeFile( path, JSON.stringify(page) , ( err ) => {
-                        if ( err ) throw err;
-                    } )
-                });
-            });
+            generateJsonFiles(generator);
 		}
-		await routes()
-	} )
+		await routes();
+	});
+}
+
+async function generateJsonFiles(generator) {
+    const res = await axios.get(`${apiUrl}/api/page`);
+    
+    res.data.map(page => {
+        let name = page.post_link.replace(baseUrl, '');
+        name = name.replace(/\//g, '-_-');
+        name = name.replace(/\\/g, '-_-');
+        const path = (generator.nuxt.options.generate.apiCacheDir + '/' + name + '.json');
+        fs.writeFile(path, JSON.stringify(page) , (err) => {
+            if (err) throw err;
+        })
+    });
 }
