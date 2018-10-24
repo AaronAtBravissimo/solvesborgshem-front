bf397cf7ceff270d9bdc5170edea5584a5cca8dd
Henric Åkesson <henric@bravissimo.se>
2018-10-24 14:05:17 +0200

    Gjort den expanderade söken responsiv lite snabbt.

--- components/ExpandedSearch.vue
+++ components/ExpandedSearch.vue
@@ -7,7 +7,7 @@
             v-show="searchOpen"
             class="expandedSearch relative"
         >
-            <div>
+            <div class="container">
                 <div class="inner flex items-center justify-center">
                     <form
                         class="form w-full relative"
@@ -109,6 +109,8 @@ export default {
 
 <style lang="scss" scoped>
 $iconSize: 22px;
+$iconSizeSmallTablet: 18px;
+$iconSizeMobile: 16px;
 
 .fade-in-enter-active {
     transition: all 0.5s ease;
@@ -133,14 +135,28 @@ $iconSize: 22px;
 }
 .inner {
     width: 800px;
+    max-width: 100%;
     padding: 50px 0;
     margin: 0 auto;
+    @media ($smallTablet) {
+        padding: 40px 0;
+    }
+    @media ($mobile) {
+        padding: 25px 15px;
+    }
 }
 .form {
     padding: 18px 0;
     border-bottom: 2px solid rgba(255, 255, 255, 0.5);
     opacity: 0;
     transition: 0.25s;
+    @media ($smallTablet) {
+        padding: 14px 0;
+    }
+    @media ($mobile) {
+        margin: 10px 0 5px;
+        padding: 10px 0;
+    }
     .done & {
         opacity: 1;
     }
@@ -153,6 +169,16 @@ $iconSize: 22px;
     height: $iconSize;
     margin-top: -#{$iconSize / 2};
     opacity: 0.5;
+    @media ($smallTablet) {
+        width: $iconSizeSmallTablet;
+        height: $iconSizeSmallTablet;
+        margin-top: -#{$iconSizeSmallTablet / 2};
+    }
+    @media ($mobile) {
+        width: $iconSizeMobile;
+        height: $iconSizeMobile;
+        margin-top: -#{$iconSizeMobile / 2};
+    }
 }
 .searchField {
     font-size: 18px;
@@ -160,6 +186,14 @@ $iconSize: 22px;
     line-height: normal;
     color: #ffffff;
     padding-left: #{$iconSize + 15px};
+    @media ($smallTablet) {
+        font-size: 16px;
+        padding-left: #{$iconSizeSmallTablet + 15px};
+    }
+    @media ($mobile) {
+        font-size: 15px;
+        padding-left: #{$iconSizeMobile + 15px};
+    }
     &::placeholder {
         color: #ffffff;
         opacity: 0.65;
@@ -169,6 +203,20 @@ $iconSize: 22px;
     position: absolute;
     right: 30px;
     top: 30px;
+    min-width: 0;
+    @media ($smallTablet) {
+        right: 15px;
+        top: 15px;
+    }
+    @media ($mobile) {
+        right: 30px;
+        top: 10px;
+    }
+    /deep/ .label {
+        @media ($mobile) {
+            display: none;
+        }
+    }
     /deep/ .icon {
         width: 12px;
         height: 12px;
