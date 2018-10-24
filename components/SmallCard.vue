8ebf7720eb1cdac25a39f21855e1201211c8925b
Henric Åkesson <henric@bravissimo.se>
2018-10-24 17:18:39 +0200

    Gjort sökresultat och arkivsidan där SmallCard används responsivt lite snabbt.

--- components/SmallCard.vue
+++ components/SmallCard.vue
@@ -6,6 +6,7 @@
         <div class="inner">
             <div class="imageHolder">
                 <AppImage
+                    v-if="image"
                     :image="image"
                     :alt="imagealt"
                     class="image"
@@ -60,9 +61,18 @@ export default {
     background-color: #fff;
     padding: 30px 50px;
     box-shadow: 0 0 50px 0 rgba(13, 42, 56, 0.1);
+    @media ($laptop) {
+        padding: 30px 30px;
+    }
+    @media ($mobile) {
+        padding: 25px 15px;
+    }
 }
 .inner {
     display: flex;
+    @media ($smallerMobile) {
+        flex-wrap: wrap;
+    }
 }
 .imageHolder {
     flex-shrink: 0;
@@ -71,18 +81,61 @@ export default {
     border-radius: 100%;
     overflow: hidden;
     position: relative;
+    @media ($laptop) {
+        width: 80px;
+        height: 80px;
+    }
+    @media ($mobile) {
+        width: 55px;
+        height: 55px;
+    }
+    @media ($smallerMobile) {
+        margin: 0 auto 20px;
+    }
 }
 .infoHolder {
     flex-shrink: 1;
     padding-left: 60px;
+    @media ($smallDesktop) {
+        padding-left: 30px;
+    }
+    @media ($mobile) {
+        padding-left: 20px;
+    }
+    @media ($smallerMobile) {
+        padding-left: 0;
+        text-align: center;
+    }
 }
 .heading {
     margin-bottom: 18px;
+    @media ($smallDesktop) {
+        letter-spacing: 3px;
+    }
+    @media ($laptop) {
+        letter-spacing: 2px;
+        font-size: 20px;
+        margin-bottom: 15px;
+    }
+    @media ($largeTablet) {
+        letter-spacing: 1px;
+    }
+    @media ($mobile) {
+        font-size: 17px;
+        margin-bottom: 10px;
+        letter-spacing: 0.5px;
+    }
 }
 .text {
     font-size: 16px;
     line-height: 1.63;
     flex-grow: 1;
+    @media ($laptop) {
+        font-size: 15px;
+    }
+    @media ($mobile) {
+        font-size: 14px;
+    }
 }
 
 </style>
