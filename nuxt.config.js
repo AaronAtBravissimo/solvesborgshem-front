e36d8c575d3af04fe5bd27c93ed1780499b2e080
Kim Larsson <kimla89@gmail.com>
2018-10-31 10:33:42 +0100

    Testar högre interval.

--- nuxt.config.js
+++ nuxt.config.js
@@ -106,7 +106,7 @@ module.exports = {
     generate: {
         fallback: true,
         apiCacheDir: 'static/json/',
-        interval: 100,
+        interval: 200,
         routes() {
             return getRoutes();
         },
