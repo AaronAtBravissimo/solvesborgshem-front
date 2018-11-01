566c80c255574521b557033913f122de5b5c67e6
Kim Larsson <kimla89@gmail.com>
2018-11-01 11:14:26 +0100

    Skapat tab komponent som används på Employees och ska användas på flera senare.

---
+++ components/Tabs.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="tabs flex justify-center">
+        <div
+            v-for="(tab, index) in tabs"
+            :key="tab.term_id"
+            class="tab"
+        >
+            <AppButton
+                :class="{'notActive': active !== index}"
+                :label="tab.name"
+                @clicked="changeTab(index)"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+import AppButton from './AppButton.vue';
+
+export default {
+    components: {
+        AppButton,
+    },
+    props: {
+        tabs: {
+            type: Array,
+            required: true,
+        },
+        active: {
+            type: Number,
+            required: true,
+        },
+    },
+    methods: {
+        changeTab(index) {
+            if (this.active !== index) {
+                this.$emit('changed', index);
+            }
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.tabs {
+    margin-bottom: 60px;
+}
+.tab {
+    margin: 0 10px;
+}
+.button.notActive {
+    background-color: #ddd;
+}
+</style>
