bbeaeaaa0e2ae8a188c227078f87ded6f6035f12
Kim Larsson <kimla89@gmail.com>
2018-11-01 15:35:10 +0100

    Riktig centrering av markers istället för hårdkodad.

--- components/BuildingsArchive.vue
+++ components/BuildingsArchive.vue
@@ -24,7 +24,7 @@
             </template>
         </SearchFilterBox>
         <BuildingsArchiveMap
-            v-show="showMap"
+            v-show="!showMap"
             :areas="areas"
             @changed="filterChanged"
         />
