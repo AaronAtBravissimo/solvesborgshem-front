bf6a148945dccccdf0f1880fd05140e8fa1eeebc
Henric Åkesson <henric@bravissimo.se>
2018-11-12 10:18:50 +0100

    Snyggt till "snabbhoppet" via tab någorlunda.

--- components/AccessibilityLinks.vue
+++ components/AccessibilityLinks.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="">
-        <div class="container">
+        <div class="container flex justify-center">
             <a
                 tabindex="1"
                 class="link"
@@ -19,14 +19,40 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$gutter: 20px;
+
 .link {
+    font-family: $secondaryFont;
     display: block;
+    font-size: 16px;
+    font-weight: 500;
+    letter-spacing: 0.5px;
+    position: relative;
     height: 0;
     opacity: 0;
+    z-index: -1;
+    color: $primaryTextColor;
+    &::after {
+        content: '';
+        position: absolute;
+        left: 0;
+        top: calc(100% - $gutter);
+        margin-top: 2px;
+        width: 100%;
+        height: 1px;
+        background-color: $primaryTextColor;
+        opacity: 0;
+        transition: 0.25s 0.25s;
+    }
 }
 .link:focus {
     height: auto;
     opacity: 1;
-    padding: 15px 0;
+    z-index: 1;
+    padding: $gutter 0;
+    outline: 0;
+    &::after {
+        opacity: 1;
+    }
 }
 </style>
