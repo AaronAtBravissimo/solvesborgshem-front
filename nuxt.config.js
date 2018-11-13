40e7c5246e52af027935fcbfdd9b4a380fe7a8e7
Henric Åkesson <henric@bravissimo.se>
2018-11-13 17:35:02 +0100

    Plockat bort detta eftersom det ändå inte verkade fungera.

--- nuxt.config.js
+++ nuxt.config.js
@@ -217,18 +217,6 @@ module.exports = {
                     }
               })
             }
-            if (!isClient) {
-                // This instructs Webpack to include `vue2-google-maps`'s Vue files
-                // for server-side rendering
-                config.externals = config.externals || [];
-                config.externals.splice(0, 0, (context, request, callback) => {
-                    if (/^vue2-google-maps($|\/)/.test(request)) {
-                        callback(null, false);
-                    } else {
-                        callback();
-                    }
-                });
-            }
         },
     },
 };
