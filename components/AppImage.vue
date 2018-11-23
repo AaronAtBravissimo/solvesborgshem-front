2e5bd29875cbeed6ac747d969b91566c03de9716
Henric Åkesson <henric@bravissimo.se>
2018-11-23 16:49:17 +0100

    Lagt in lazyloading på bilder.

--- components/AppImage.vue
+++ components/AppImage.vue
@@ -1,13 +1,15 @@
 <template>
     <picture>
         <source
-            :srcset="`${image}.webp`"
+            :srcset="`${imageSrc}.webp`"
             type="image/webp"
         >
         <img
-            :src="image"
+            ref="image"
+            :src="imageSrc"
             :alt="alt"
-            class="ab100 objectFitCover"
+            :class="{ 'loaded': isLoaded }"
+            class="imageSrc ab100 objectFitCover"
         >
     </picture>
 </template>
@@ -23,6 +25,41 @@ export default {
             type: String,
             default: '',
         },
+        lazy: {
+            type: Boolean,
+            default: true,
+        },
+    },
+    data: () => ({
+        loaded: false,
+    }),
+    computed: {
+        isLoaded() {
+            return !this.lazy || this.loaded;
+        },
+        imageSrc() {
+            if (this.lazy && !this.loaded) return null;
+
+            return this.image;
+        },
+    },
+    mounted() {
+        if (!process.client || !this.lazy) return;
+
+        const observer = new IntersectionObserver(this.handleIntersect, {
+            rootMargin: '1200px 0px 1200px 0px',
+            threshold: [0, 0.05, 0.01],
+        });
+
+        observer.observe(this.$refs.image);
+    },
+    methods: {
+        handleIntersect(entries, observer) {
+            if (entries[0].intersectionRatio > 0) {
+                this.loaded = true;
+                observer.unobserve(this.$refs.image);
+            }
+        },
     },
 };
 </script>
@@ -31,5 +68,11 @@ export default {
 .image {
     display: block;
     width: 100%;
+}
+.imageSrc {
+    opacity: 0;
+}
+.loaded {
+    opacity: 1;
 }
 </style>
